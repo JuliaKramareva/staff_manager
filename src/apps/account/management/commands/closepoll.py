45a824caad99e4f352b8df2d7d2df968b34ef719
Julia Kramareva <forjobonly100@gmail.com>
2018-12-27 01:15:20 +0200

    new update

--- src/apps/account/management/commands/closepoll.py
+++ src/apps/account/management/commands/closepoll.py
@@ -7,21 +7,20 @@ import names
 
 
 
-# TODO python manage.py create_test_data
-
 
 class Command(BaseCommand):
     help = 'Create test data'
     # startdate = datetime.date(YYYY,MM,DD)
-    domains = ["hotmail.com", "gmail.com", "mail.com", "yahoo.com"]
+    domains = ["hotmail.com", "gmail.com", "mail.com", "yahoo.com", "icloud.com"]
     letters = string.ascii_lowercase[:12]
+
 
 
     def handle(self, *args, **options):
         User.objects.exclude(is_superuser=True).delete()
         for i in range(10_000):
             user = User.objects.create(
-                id = id.random.choice(1, 10000),
+                id = id.random.choice(1, 10_000),
                 password = password.random.choice(string.ascii_lowercase + string.digits),
                 last_login = last_login.datetime.date(random.randint(2018,2018), random.randint(1,12), \
                                                       random.randint(1,28)),
@@ -31,12 +30,10 @@ class Command(BaseCommand):
                 first_name = first_name.get_first_name(),
                 last_name = last_name.get_last_name(),
                 email = email.  ,
-                is_staff = is_staff.    ,
-                is_active = is_active.   ,
-                date_joined = date_joined.datetime.date(randint(2017,2018), randint(1,12),randint(1,28)),
-                age = age. ,
+                is_staff = True,
+                is_active = True,
+                date_joined = date_joined.datetime.date(random.randint(2017,2018), random.randint(1,12), random.randint(1,28)),
+                age = age.random.choice(21, 65),
                 groups = groups.  ,
                 user_permissions = user_permissions.
-
-
             )
