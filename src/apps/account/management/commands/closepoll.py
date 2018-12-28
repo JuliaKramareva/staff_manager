7dc7b4d80684297a84f8dd0bcf51cf27286e1ba5
Julia Kramareva <forjobonly100@gmail.com>
2018-12-28 04:35:50 +0200

    added updates

--- src/apps/account/management/commands/closepoll.py
+++ src/apps/account/management/commands/closepoll.py
@@ -10,9 +10,13 @@ import names
 
 class Command(BaseCommand):
     help = 'Create test data'
-    # startdate = datetime.date(YYYY,MM,DD)
+
     domains = ["hotmail.com", "gmail.com", "mail.com", "yahoo.com", "icloud.com"]
     letters = string.ascii_lowercase[:12]
+    username_choice = (''.join([random.choice(string.ascii_lowercase) for x in range(random.choice(1, 10)]) + \
+                                     ''.join([random.choice(string.digits) for z in range(random.choice(1, 10)]))
+    permissions = (("Can view user", "Can view content type", "Can view group", "Can view session"), )
+    GROUPS_STAFF = ("Administration", "Analytics", "Statisticians", "Developers", "Sales")
 
 
 
@@ -20,20 +24,19 @@ class Command(BaseCommand):
         User.objects.exclude(is_superuser=True).delete()
         for i in range(10_000):
             user = User.objects.create(
-                id = id.random.choice(1, 10_000),
                 password = password.random.choice(string.ascii_lowercase + string.digits),
                 last_login = last_login.datetime.date(random.randint(2018,2018), random.randint(1,12), \
                                                       random.randint(1,28)),
                 is_superuser = False,
-                username = username.(''.join(random.choice(string.ascii_lowercase) for x in range(random.choice(1, 10))) + \
-                                     ''.join(random.choice(string.digits) for z in range(random.choice(1, 100)))),
+                username = username.randome.choice(username_choice),
                 first_name = first_name.get_first_name(),
                 last_name = last_name.get_last_name(),
-                email = email.  ,
+                email = email.random.choice(last_name.get_last_name() + "@" + domains),
                 is_staff = True,
                 is_active = True,
-                date_joined = date_joined.datetime.date(random.randint(2017,2018), random.randint(1,12), random.randint(1,28)),
+                date_joined = date_joined.datetime.date(random.randint(2017,2018), random.randint(1,12), \
+                                                        random.randint(1,28)),
                 age = age.random.choice(21, 65),
-                groups = groups.  ,
-                user_permissions = user_permissions.
+                groups = groups.random.choice(GROUPS_STAFF),
+                user_permissions = user_permissions.permissions
             )
